fd2b414e6a8c1fa7b1521499bbc56db005ca4a79
Vadym <vadymkolomi@gmail.com>
2026-03-30 08:52:38 -0400

    add google vision

--- apps/ocr-worker/src/app/ocr/vision.service.ts
+++ apps/ocr-worker/src/app/ocr/vision.service.ts
@@ -1,24 +1,36 @@
-import {Injectable, Logger} from "@nestjs/common";
+import {Injectable, InternalServerErrorException, Logger} from "@nestjs/common";
 import {OcrParsedResult} from "./interfaces/ocr-job.interface";
+import {ImageAnnotatorClient} from "@google-cloud/vision";
 
 @Injectable()
 export class VisionService {
   private readonly logger = new Logger(VisionService.name);
-
+  private readonly client = new ImageAnnotatorClient();
 
   async extractText(imageBuffer: Buffer): Promise<OcrParsedResult> {
-    this.logger.log(`Analyzing image buffer of size: ${imageBuffer.length} bytes`);
-    await new Promise(resolve => setTimeout(resolve, 5000));
+    this.logger.log(`Sending image (${imageBuffer.length} bytes) to Google Vision API...`);
+    try {
+      const [result] = await this.client.documentTextDetection(imageBuffer);
+      const rawText = result.fullTextAnnotation?.text || '';
+      if (!rawText) {
+        throw new Error('Google Vision returned no text.');
+      }
 
-    //TODO: replace with real @google-cloud/vision SDK logic
-    const mockExtractedText = "KFC \n Total: 15.99 \n Date: 2026-03-30";
-    this.logger.log('OCR text extraction complete. Parsing financial data...');
+      this.logger.log('Successfully extracted raw text from Google Vision.');
+      return this.parseRawText(rawText);
+
+    } catch (e) {
+      this.logger.error('Google Vision API failed', e.stack);
+      throw new InternalServerErrorException('Failed to process image with AI');
+    }
+  }
+  private parseRawText(rawText: string): OcrParsedResult {
+    // TODO:we will build a smart parser here to find the Merchant, Total, and Date  from the chaotic raw text string
     return {
-      merchantName: 'KFC',
-      totalAmount: 15.99,
+      merchantName: 'Unknown Merchant',
+      totalAmount: 0,
       date: new Date(),
-      rawText: mockExtractedText
+      rawText: rawText,
     };
   }
-
 }
