c228304b6124d59a909597b159f57592825eb66c
Vadym <vadymkolomi@gmail.com>
2026-03-30 06:34:15 -0400

    enqueue the Job in the receipts service

--- apps/api/src/app/receipts/receipt.module.ts
+++ apps/api/src/app/receipts/receipt.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { ReceiptController } from './receipt.controller';
 import { ReceiptService } from './receipt.service';
 import { PrismaModule } from '@src/prisma';
+import {BullModule} from "@nestjs/bullmq";
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    BullModule.registerQueue({
+      name: 'ocr-jobs',
+    }),
+  ],
   controllers: [ReceiptController],
   providers: [ReceiptService],
 })
--- apps/api/src/app/receipts/receipt.service.ts
+++ apps/api/src/app/receipts/receipt.service.ts
@@ -2,20 +2,23 @@ import 'multer';
 import { Injectable, Logger } from '@nestjs/common';
 import {PrismaService} from "@src/prisma";
 import {StorageService} from "@src/storage";
+import {InjectQueue} from "@nestjs/bullmq";
+import {Queue} from "bullmq";
 
 @Injectable()
 export class ReceiptService {
   private readonly logger = new Logger(ReceiptService.name);
   constructor(
     private readonly prisma: PrismaService,
-    private readonly storageService: StorageService
+    private readonly storageService: StorageService,
+    @InjectQueue('ocr-jobs') private readonly ocrQueue: Queue
   ) {}
 
   async processUpload(file:Express.Multer.File, userId:string) {
     this.logger.log(`Processing receipt upload: ${userId}, file: ${file.originalname}`);
-
+    //upload s3
     const storageKey = await this.storageService.uploadFile(file, userId);
-
+    //create DB record
     const receipt = await this.prisma.receipt.create({
       data: {
         userId: userId,
@@ -35,6 +38,13 @@ export class ReceiptService {
       }
     });
     this.logger.log(`Receipt and image records created successfully: ${receipt.id}`);
+    //enqueue ocr job
+    await this.ocrQueue.add('process-receipt', {
+      receiptId:receipt.id,
+      imageId:receipt.images[0].id,
+      storageKey:storageKey,
+    })
+    this.logger.log(`Successfully enqueued OCR job for receipt: ${receipt.id}`);
 
     return {
       message: 'Receipt uploaded successfully. OCR processing pending.',
